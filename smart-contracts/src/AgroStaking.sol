7c25e216ac88c0b7572d95c92ca39f772f8da097
Andrei Iarovoi <iarovoiandrei02@gmail.com>
2026-06-04 19:30:33 +0200

    chore: add Prettier config + plugin for Solidity; format .sol files

--- smart-contracts/src/AgroStaking.sol
+++ smart-contracts/src/AgroStaking.sol
@@ -1,60 +1,90 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
+import {Pausable} from '@openzeppelin/contracts/utils/Pausable.sol';
+import {ReentrancyGuard} from '@openzeppelin/contracts/utils/ReentrancyGuard.sol';
 
 /// @title AgroStake
 /// @author Andrei Iarovoi
 /// @notice Staking contract for AGRO token
-contract AgroStaking is Ownable, Pausable, ReentrancyGuard { 
-    /// @notice Annual percentage rate
-    uint256 public constant APR = 12;
+contract AgroStaking is Ownable, Pausable, ReentrancyGuard {
+  /// @notice Annual percentage rate
+  uint256 public constant APR = 12;
 
-    /// @notice Percentage denominator
-    uint256 public constant BASIS_POINTS = 100;
+  /// @notice Percentage denominator
+  uint256 public constant BASIS_POINTS = 100;
 
-    /// @notice Seconds in one year
-    uint256 public constant YEAR_IN_SECONDS = 365 days;
+  /// @notice Seconds in one year
+  uint256 public constant YEAR_IN_SECONDS = 365 days;
 
-    /// @notice Minimum staking lock period
-    uint256 public constant LOCK_PERIOD = 7 days;
+  /// @notice Minimum staking lock period
+  uint256 public constant LOCK_PERIOD = 7 days;
 
-    /// @notice AGRO token contract
-    IERC20 public immutable stakingToken;
+  /// @notice AGRO token contract
+  IERC20 public immutable stakingToken;
 
-    struct StakeInfo {
-        uint256 amount;
-        uint256 rewardsClaimed;
-        uint256 unclaimedRewards;
-        uint256 lastRewardTimestamp;
-        uint256 stakeTimestamp;
+  struct StakeInfo {
+    uint256 amount;
+    uint256 rewardsClaimed;
+    uint256 unclaimedRewards;
+    uint256 lastRewardTimestamp;
+    uint256 stakeTimestamp;
+  }
+
+  mapping(address => StakeInfo) public stakes;
+
+  uint256 public totalStaked;
+  uint256 public totalStakers;
+
+  error AmountMustBeGreaterThanZero();
+  error InsufficientStake();
+  error LockPeriodNotExpired();
+  error NoRewardsAvailable();
+
+  event Staked(address indexed user, uint256 amount);
+  event Unstaked(address indexed user, uint256 amount);
+  event RewardsClaimed(address indexed user, uint256 reward);
+  event EmergencyWithdraw(address indexed user, uint256 amount);
+
+  /// @param tokenAddress AGRO token address
+  constructor(address tokenAddress, address initialOwner) Ownable(initialOwner) {
+    stakingToken = IERC20(tokenAddress);
+  }
+
+  /// @notice Stake AGRO tokens into the protocol
+  /// @param amount Amount of tokens to stake
+  function stake(uint256 amount) external whenNotPaused nonReentrant {
+    if (amount == 0) {
+      revert AmountMustBeGreaterThanZero();
     }
 
-    mapping(address => StakeInfo) public stakes;
+    StakeInfo storage userStake = stakes[msg.sender];
 
-    uint256 public totalStaked;
-    uint256 public totalStakers;
+    _updateRewards(msg.sender);
 
-    error AmountMustBeGreaterThanZero();
-    error InsufficientStake();
-    error LockPeriodNotExpired();
-    error NoRewardsAvailable();
+    bool isNewStaker = userStake.amount == 0;
 
-    event Staked(address indexed user, uint256 amount);
-    event Unstaked(address indexed user, uint256 amount);
-    event RewardsClaimed(address indexed user, uint256 reward);
-    event EmergencyWithdraw(address indexed user, uint256 amount);
+    stakingToken.transferFrom(msg.sender, address(this), amount);
 
-    /// @param tokenAddress AGRO token address
-    constructor(
-        address tokenAddress,
-        address initialOwner
-    )
-        Ownable(initialOwner)
-    {
-        stakingToken = IERC20(tokenAddress);
+    userStake.amount += amount;
+
+    if (isNewStaker) {
+      totalStakers++;
+      userStake.stakeTimestamp = block.timestamp;
     }
+
+    userStake.lastRewardTimestamp = block.timestamp;
+
+    totalStaked += amount;
+
+    emit Staked(msg.sender, amount);
+  }
+
+  /// @notice Updates user's accumulated rewards
+  /// @param user Address of the staker
+  function _updateRewards(address user) private {
+    user;
+  }
 }
--- smart-contracts/src/AgroToken.sol
+++ smart-contracts/src/AgroToken.sol
@@ -1,59 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 
 /// @title Agro Token
 /// @author Andrei Iarovoi
 /// @notice ERC20 token used for staking in the AgroStake protocol
 contract AgroToken is ERC20, Ownable {
-    /// @notice Amount of AGRO distributed by the faucet
-    uint256 public constant FAUCET_AMOUNT = 1_000 ether;
+  /// @notice Amount of AGRO distributed by the faucet
+  uint256 public constant FAUCET_AMOUNT = 1_000 ether;
 
-    /// @notice Cooldown period between faucet claims
-    uint256 public constant FAUCET_COOLDOWN = 1 days;
+  /// @notice Cooldown period between faucet claims
+  uint256 public constant FAUCET_COOLDOWN = 1 days;
 
-    /// @notice Timestamp of the user's last faucet claim
-    mapping(address => uint256) public lastFaucetClaim;
+  /// @notice Timestamp of the user's last faucet claim
+  mapping(address => uint256) public lastFaucetClaim;
 
-    /// @notice Thrown when faucet cooldown has not expired
-    error FaucetCooldownActive();
+  /// @notice Thrown when faucet cooldown has not expired
+  error FaucetCooldownActive();
 
-    /// @notice Emitted when a user claims faucet tokens
-    /// @param user Address claiming tokens
-    /// @param amount Amount of tokens received
-    event FaucetClaimed(address indexed user, uint256 amount);
+  /// @notice Emitted when a user claims faucet tokens
+  /// @param user Address claiming tokens
+  /// @param amount Amount of tokens received
+  event FaucetClaimed(address indexed user, uint256 amount);
 
-    /// @param initialOwner Owner of the contract
-    constructor(address initialOwner)
-        ERC20("Agro Token", "AGRO")
-        Ownable(initialOwner)
-    {}
+  /// @param initialOwner Owner of the contract
+  constructor(address initialOwner) ERC20('Agro Token', 'AGRO') Ownable(initialOwner) {}
 
-    /// @notice Mint AGRO tokens
-    /// @dev Only contract owner can mint
-    /// @param to Recipient address
-    /// @param amount Amount of tokens to mint
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
+  /// @notice Mint AGRO tokens
+  /// @dev Only contract owner can mint
+  /// @param to Recipient address
+  /// @param amount Amount of tokens to mint
+  function mint(address to, uint256 amount) external onlyOwner {
+    _mint(to, amount);
+  }
+
+  /// @notice Claim free AGRO test tokens
+  /// @dev Can only be called once per cooldown period
+  function faucet() external {
+    if (
+      lastFaucetClaim[msg.sender] != 0 &&
+      block.timestamp < lastFaucetClaim[msg.sender] + FAUCET_COOLDOWN
+    ) {
+      revert FaucetCooldownActive();
     }
 
-    /// @notice Claim free AGRO test tokens
-    /// @dev Can only be called once per cooldown period
-    function faucet() external {
-        if (
-            lastFaucetClaim[msg.sender] != 0 &&
-            block.timestamp <
-            lastFaucetClaim[msg.sender] + FAUCET_COOLDOWN
-            ) {
-        revert FaucetCooldownActive();
-        }
+    lastFaucetClaim[msg.sender] = block.timestamp;
 
-        lastFaucetClaim[msg.sender] = block.timestamp;
+    _mint(msg.sender, FAUCET_AMOUNT);
 
-        _mint(msg.sender, FAUCET_AMOUNT);
-
-        emit FaucetClaimed(msg.sender, FAUCET_AMOUNT);
-    }
+    emit FaucetClaimed(msg.sender, FAUCET_AMOUNT);
+  }
 }
