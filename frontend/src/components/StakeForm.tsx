036e3c3a4b570ec8132c990a6162aa04016be8f5
Andrei Iarovoi <iarovoiandrei02@gmail.com>
2026-06-12 13:30:36 +0200

    feat: add staking transaction

--- frontend/src/components/StakeForm.tsx
+++ frontend/src/components/StakeForm.tsx
@@ -1,7 +1,44 @@
 import { useState } from 'react';
 
+import { parseEther } from 'viem';
+
+import { useWriteContract } from 'wagmi';
+
+import { AGRO_TOKEN_ADDRESS, AGRO_STAKING_ADDRESS } from '../contracts/addresses';
+
+import { agroTokenAbi } from '../contracts/agroToken';
+import { agroStakingAbi } from '../contracts/agroStaking';
+
 export function StakeForm() {
+  const { writeContract } = useWriteContract();
+
   const [amount, setAmount] = useState('');
+
+  function handleApprove() {
+    if (!amount) return;
+
+    writeContract({
+      account: undefined,
+      chain: undefined,
+      address: AGRO_TOKEN_ADDRESS,
+      abi: agroTokenAbi,
+      functionName: 'approve',
+      args: [AGRO_STAKING_ADDRESS, parseEther(amount)],
+    });
+  }
+
+  function handleStake() {
+    if (!amount) return;
+
+    writeContract({
+      account: undefined,
+      chain: undefined,
+      address: AGRO_STAKING_ADDRESS,
+      abi: agroStakingAbi,
+      functionName: 'stake',
+      args: [parseEther(amount)],
+    });
+  }
 
   return (
     <div>
@@ -13,6 +50,8 @@ export function StakeForm() {
         onChange={(e) => setAmount(e.target.value)}
         placeholder="Amount"
       />
+      <button onClick={handleApprove}>Approve</button>
+      <button onClick={handleStake}>Stake</button>
     </div>
   );
 }
